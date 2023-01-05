0c21c9557b5c8855e92cd6a3e5ef4fce9aaab571
Gabriel Bernal <gbernal@redhat.com>
2023-01-05 20:52:35 +0100

    add plugin backend

---
+++ go.mod
@@ -0,0 +1,20 @@
+module github.com/openshift/logging-view-plugin
+
+go 1.18
+
+require (
+	github.com/evanphx/json-patch v0.5.2
+	github.com/gorilla/handlers v1.5.1
+	github.com/gorilla/mux v1.8.0
+	github.com/sirupsen/logrus v1.9.0
+	github.com/stretchr/testify v1.8.1
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/felixge/httpsnoop v1.0.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/sys v0.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ pkg/server/server.go
@@ -0,0 +1,100 @@
+package server
+
+import (
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
+)
+
+var slog = logrus.WithField("module", "server")
+
+type Config struct {
+	Port           int
+	CertFile       string
+	PrivateKeyFile string
+	Features       map[string]bool
+	StaticPath     string
+	ConfigPath     string
+}
+
+func Start(cfg *Config) {
+	router := setupRoutes(cfg)
+	router.Use(corsHeaderMiddleware(cfg))
+
+	loggedRouter := handlers.LoggingHandler(slog.Logger.Out, router)
+
+	// clients must use TLS 1.2 or higher
+	tlsConfig := &tls.Config{
+		MinVersion: tls.VersionTLS12,
+	}
+
+	httpServer := &http.Server{
+		Handler:      loggedRouter,
+		Addr:         fmt.Sprintf(":%d", cfg.Port),
+		TLSConfig:    tlsConfig,
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	if cfg.CertFile != "" && cfg.PrivateKeyFile != "" {
+		slog.Infof("listening on https://:%d", cfg.Port)
+		panic(httpServer.ListenAndServeTLS(cfg.CertFile, cfg.PrivateKeyFile))
+	} else {
+		slog.Infof("listening on http://:%d", cfg.Port)
+		panic(httpServer.ListenAndServe())
+	}
+}
+
+func setupRoutes(cfg *Config) *mux.Router {
+	r := mux.NewRouter()
+
+	r.PathPrefix("/health").HandlerFunc(healthHandler())
+
+	// serve plugin manifest according to enabled features
+	r.Path("/plugin-manifest.json").Handler(manifestHandler(cfg))
+
+	// serve enabled features list to the front-end
+	r.PathPrefix("/features").HandlerFunc(featuresHandler(cfg))
+
+	// serve front end files
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir(cfg.StaticPath)))
+
+	return r
+}
+
+func healthHandler() http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("ok"))
+	})
+}
+
+func corsHeaderMiddleware(cfg *Config) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			headers := w.Header()
+			headers.Set("Access-Control-Allow-Origin", "*")
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+func featuresHandler(cfg *Config) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		jsonFeatures, err := json.Marshal(cfg.Features)
+
+		if err != nil {
+			slog.WithError(err).Errorf("cannot unmarshal, features were: %v", string(jsonFeatures))
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(jsonFeatures)
+	})
+}
