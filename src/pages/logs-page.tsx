b372d6d19b21c9fca381a2faf92b872259700bac
Gabriel Bernal <gbernal@redhat.com>
2023-04-03 12:18:33 +0200

    make load more logs consistent with the sorting direction

--- src/pages/logs-page.tsx
+++ src/pages/logs-page.tsx
@@ -165,6 +165,7 @@ const LogsPage: React.FC = () => {
           isLoadingMore={isLoadingMoreLogsData}
           hasMoreLogsData={hasMoreLogsData}
           showResources={areResourcesShown}
+          direction={direction}
           isStreaming={isStreaming}
           error={logsError}
         >
