b800732250769cef25db80003831209171121b13
Gabriel Bernal <gbernal@redhat.com>
2024-08-19 14:08:37 +0200

    make correlation links aware of dev perspective

--- web/mockModules/dummy.tsx
+++ web/mockModules/dummy.tsx
@@ -2,3 +2,4 @@ export { ResourceLink } from '@openshift-console/dynamic-plugin-sdk';
 export { WSFactory } from '@openshift-console/dynamic-plugin-sdk/lib/utils/k8s/ws-factory';
 
 export const useK8sWatchResource = () => [null, true, ''];
+export const useActivePerspective = () => ['admin'];
