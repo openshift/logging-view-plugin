bce05323425ab077f97b0feed3d69364fd24188b
Gabriel Bernal <gbernal@redhat.com>
2024-12-10 12:12:39 +0100

    split large histogram queries into multiple small sub queries

--- web/src/cancellable-fetch.ts
+++ web/src/cancellable-fetch.ts
@@ -46,33 +46,35 @@ export const cancellableFetch = <T>(
   const abortController = new AbortController();
   const abort = () => abortController.abort();
 
-  const fetchPromise = fetch(url, {
-    ...init,
-    headers: {
-      ...init?.headers,
-      Accept: 'application/json',
-      ...(init?.method === 'POST' ? { 'X-CSRFToken': getCSRFToken() } : {}),
-    },
-    signal: abortController.signal,
-  }).then(async (response) => {
-    if (!response.ok) {
-      const text = await response.text();
-      throw new FetchError(text, response.status);
-    }
-    return response.json();
-  });
+  const fetchPromise = () =>
+    fetch(url, {
+      ...init,
+      headers: {
+        ...init?.headers,
+        Accept: 'application/json',
+        ...(init?.method === 'POST' ? { 'X-CSRFToken': getCSRFToken() } : {}),
+      },
+      signal: abortController.signal,
+    }).then(async (response) => {
+      if (!response.ok) {
+        const text = await response.text();
+        throw new FetchError(text, response.status);
+      }
+      return response.json();
+    });
 
   const timeout = init?.timeout ?? 30 * 1000;
 
   if (timeout <= 0) {
-    return { request: () => fetchPromise, abort };
+    return { request: fetchPromise, abort };
   }
 
-  const timeoutPromise = new Promise<T>((_resolve, reject) => {
-    setTimeout(() => reject(new TimeoutError(url.toString(), timeout)), timeout);
-  });
+  const timeoutPromise = () =>
+    new Promise<T>((_resolve, reject) => {
+      setTimeout(() => reject(new TimeoutError(url.toString(), timeout)), timeout);
+    });
 
-  const request = () => Promise.race([fetchPromise, timeoutPromise]);
+  const request = () => Promise.race([fetchPromise(), timeoutPromise()]);
 
   return { request, abort };
 };
