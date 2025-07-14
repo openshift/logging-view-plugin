a0952324a4d77fd7133b43e4d389773592b815db
anpingli <anli@redhat.com>
2025-07-14 15:23:58 +0800

    merge with openshift-test-private and upgrade cypress

---
+++ web/upstream/views/details-page.ts
@@ -0,0 +1,26 @@
+export const detailsPage = {
+  titleShouldContain: (title: string) => cy.byLegacyTestID('resource-title').contains(title),
+  sectionHeaderShouldExist: (sectionHeading: string) =>
+    cy.get(`[data-test-section-heading="${sectionHeading}"]`).should('exist'),
+  labelShouldExist: (labelName: string) => cy.byTestID('label-list').contains(labelName),
+  clickPageActionFromDropdown: (actionID: string) => {
+    cy.byLegacyTestID('actions-menu-button').click();
+    cy.byTestActionID(actionID).click();
+  },
+  clickPageActionButton: (action: string) => {
+    cy.byLegacyTestID('details-actions').contains(action).click();
+  },
+  isLoaded: () => cy.byTestID('skeleton-detail-view').should('not.exist'),
+  breadcrumb: (breadcrumbIndex: number) => cy.byLegacyTestID(`breadcrumb-link-${breadcrumbIndex}`),
+  selectTab: (name: string) => {
+    cy.get(`a[data-test-id="horizontal-link-${name}"]`).should('exist').click();
+  },
+};
+
+export namespace DetailsPageSelector {
+  export const name = 'dd[data-test-selector="details-item-value__Name"]';
+  export const namespace = 'dd[data-test-selector="details-item-value__Namespace"] a';
+  export const sectionHeadings = '[data-test-section-heading]';
+  export const itemLabels = 'dt';
+  export const horizontalNavTabs = '.co-m-horizontal-nav__menu-item';
+}
