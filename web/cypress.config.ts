5af077b2212ce95b1c212317de2e8ae67441fccf
Gabriel Bernal <gbernal@redhat.com>
2025-11-19 10:42:11 +0100

    fix: remove unused cypress coverage package
    Signed-off-by: Gabriel Bernal <gbernal@redhat.com>

--- web/cypress.config.ts
+++ web/cypress.config.ts
@@ -3,11 +3,6 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:9003',
-    setupNodeEvents(on, config) {
-      // eslint-disable-next-line @typescript-eslint/no-var-requires
-      require('@cypress/code-coverage/task')(on, config);
-      return config;
-    },
   },
   video: false,
   viewportWidth: 1400,
