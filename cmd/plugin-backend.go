72ee8e771bf888e117068b28e7c9a70211e7afdd
Jenny Zhu <jenny.a.zhu@gmail.com>
2026-04-07 13:41:30 -0400

    fix: COO-1587 Implement TLS. Refactor to use k8sapiflag.

--- cmd/plugin-backend.go
+++ cmd/plugin-backend.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"crypto/tls"
 	"flag"
 	"os"
 	"strconv"
@@ -10,6 +9,7 @@ import (
 
 	"github.com/openshift/logging-view-plugin/pkg/server"
 	"github.com/sirupsen/logrus"
+	k8sapiflag "k8s.io/component-base/cli/flag"
 )
 
 var (
@@ -21,9 +21,8 @@ var (
 	configPathArg      = flag.String("config-path", "", "config files path (default: './config')")
 	pluginConfigArg    = flag.String("plugin-config-path", "", "plugin yaml configuration")
 	logLevelArg        = flag.String("log-level", logrus.InfoLevel.String(), "verbosity of logs\noptions: ['panic', 'fatal', 'error', 'warn', 'info', 'debug', 'trace']\n'trace' level will log all incoming requests\n(default 'error')")
-	tlsMinVersionArg   = flag.String("tls-min-version", "", "minimum TLS version\noptions: ['VersionTLS10', 'VersionTLS11', 'VersionTLS12', 'VersionTLS13']\n(default 'VersionTLS12')")
-	tlsMaxVersionArg   = flag.String("tls-max-version", "", "maximum TLS version\noptions: ['VersionTLS10', 'VersionTLS11', 'VersionTLS12', 'VersionTLS13']\n(default is the highest supported by Go)")
-	tlsCipherSuitesArg = flag.String("tls-cipher-suites", "", "comma-separated list of cipher suites for the server\nvalues are from tls package constants (https://golang.org/pkg/crypto/tls/#pkg-constants)")
+	tlsMinVersionArg   = flag.String("tls-min-version", "VersionTLS12", "minimum TLS version supported. Values are from tls package constants (https://golang.org/pkg/crypto/tls/#pkg-constants)")
+	tlsCipherSuitesArg = flag.String("tls-cipher-suites", "", "comma-separated list of cipher suites for the server.\nValues are from tls package constants (https://golang.org/pkg/crypto/tls/#pkg-constants).\nIf omitted, the default Go cipher suite will be used")
 	log                = logrus.WithField("module", "main")
 )
 
@@ -38,8 +37,7 @@ func main() {
 	configPath := mergeEnvValue("LOGGING_VIEW_PLUGIN_MANIFEST_CONFIG_PATH", *configPathArg, "./config")
 	pluginConfigPath := mergeEnvValue("LOGGING_VIEW_PLUGIN_CONFIG_PATH", *pluginConfigArg, "/etc/plugin/config.yaml")
 	logLevel := mergeEnvValue("LOGGING_VIEW_PLUGIN_LOG_LEVEL", *logLevelArg, logrus.InfoLevel.String())
-	tlsMinVersion := mergeEnvValue("TLS_MIN_VERSION", *tlsMinVersionArg, "")
-	tlsMaxVersion := mergeEnvValue("TLS_MAX_VERSION", *tlsMaxVersionArg, "")
+	tlsMinVersion := mergeEnvValue("TLS_MIN_VERSION", *tlsMinVersionArg, "VersionTLS12")
 	tlsCipherSuites := mergeEnvValue("TLS_CIPHER_SUITES", *tlsCipherSuitesArg, "")
 
 	featuresList := strings.Fields(strings.Join(strings.Split(strings.ToLower(features), ","), " "))
@@ -59,10 +57,24 @@ func main() {
 
 	log.Infof("enabled features: %+q\n", featuresList)
 
-	// Parse TLS configuration
-	tlsMinVer := parseTLSVersion(tlsMinVersion)
-	tlsMaxVer := parseTLSVersion(tlsMaxVersion)
-	tlsCiphers := parseCipherSuites(tlsCipherSuites)
+	// Parse TLS configuration using k8sapiflag
+	tlsMinVer, err := k8sapiflag.TLSVersion(tlsMinVersion)
+	if err != nil {
+		log.Warnf("Invalid TLS version %q: %v, using default VersionTLS12", tlsMinVersion, err)
+		tlsMinVer = k8sapiflag.DefaultTLSVersion()
+	}
+
+	tlsMaxVer := uint16(0) // Use Go default for max version
+
+	var tlsCiphers []uint16
+	if tlsCipherSuites != "" {
+		cipherNames := strings.Split(strings.ReplaceAll(tlsCipherSuites, " ", ""), ",")
+		tlsCiphers, err = k8sapiflag.TLSCipherSuites(cipherNames)
+		if err != nil {
+			log.Warnf("Invalid cipher suites %q: %v, using Go defaults", tlsCipherSuites, err)
+			tlsCiphers = nil
+		}
+	}
 
 	srv, err := server.CreateServer(context.Background(), &server.Config{
 		Port:             port,
@@ -114,67 +126,3 @@ func mergeEnvValueInt(key string, arg int, defaultValue int) int {
 	return defaultValue
 }
 
-func getCipherSuitesMap() map[string]uint16 {
-	result := make(map[string]uint16)
-
-	for _, suite := range tls.CipherSuites() {
-		result[suite.Name] = suite.ID
-	}
-
-	return result
-}
-
-func getTLSVersionsMap() map[string]uint16 {
-	versions := make(map[string]uint16)
-
-	versions["VersionTLS10"] = tls.VersionTLS10
-	versions["VersionTLS11"] = tls.VersionTLS11
-	versions["VersionTLS12"] = tls.VersionTLS12
-	versions["VersionTLS13"] = tls.VersionTLS13
-
-	return versions
-}
-
-func parseTLSVersion(version string) uint16 {
-	if version == "" {
-		return tls.VersionTLS12
-	}
-
-	tlsVersions := getTLSVersionsMap()
-
-	if v, ok := tlsVersions[version]; ok {
-		return v
-	}
-
-	log.Warnf("Invalid TLS version %q, using default VersionTLS12", version)
-	return tls.VersionTLS12
-}
-
-func parseCipherSuites(ciphers string) []uint16 {
-	if ciphers == "" {
-		return nil
-	}
-
-	cipherMap := getCipherSuitesMap()
-
-	cipherNames := strings.Split(strings.ReplaceAll(ciphers, " ", ""), ",")
-	var result []uint16
-
-	for _, name := range cipherNames {
-		if name == "" {
-			continue
-		}
-		if cipher, ok := cipherMap[name]; ok {
-			result = append(result, cipher)
-		} else {
-			log.Warnf("Unknown cipher suite %q, skipping", name)
-		}
-	}
-
-	if len(result) == 0 {
-		log.Warn("No valid cipher suites provided, using Go defaults")
-		return nil
-	}
-
-	return result
-}
